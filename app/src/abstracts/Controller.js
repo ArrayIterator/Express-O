336399b50e3c74bffbf14a62ab5ed01c143b47c3
ArrayIterator <arrayiterator@gmail.com>
2024-06-25 21:29:26 +0700

    add validator

--- app/src/abstracts/Controller.js
+++ app/src/abstracts/Controller.js
@@ -9,7 +9,7 @@ import {sprintf} from "../helpers/Formatting.js";
 import {__} from "../l10n/Translator.js";
 import RuntimeException from "../errors/exceptions/RuntimeException.js";
 import {
-    is_array, is_empty,
+    is_array, is_async_function, is_empty,
     is_function,
     is_integer,
     is_numeric,
@@ -23,6 +23,7 @@ import HTTP_MESSAGES from "../helpers/HttpCode.js";
 import {filter_content_type, intval} from "../helpers/DataType.js";
 import ResponseTimeoutException from "../errors/exceptions/ResponseTimeoutException.js";
 import {ALL} from "../router/Methods.js";
+import {NormalizeHeaderKey} from "../l10n/Filter.js";
 
 /**
  * Abstract route
@@ -96,7 +97,7 @@ export default class Controller {
      * @abstract
      * @protected
      */
-    methods = [];
+    methods = [ALL];
 
     /**
      * Route Path
@@ -133,6 +134,14 @@ export default class Controller {
     __route_id;
 
     /**
+     * Safe methods
+     *
+     * @type {string[]}
+     * @private
+     */
+    #methods = [ALL];
+
+    /**
      * Constructor
      * @final
      */
@@ -155,7 +164,7 @@ export default class Controller {
             );
         }
 
-        this.methods = this.methods || [];
+        this.methods = this.methods || [ALL];
         this.methods = is_string(this.methods) ? [this.methods] : this.methods;
         // filter methods
         this.methods = this.methods.filter((method) => {
@@ -166,11 +175,65 @@ export default class Controller {
         if (is_empty(this.methods)) {
             this.methods = [ALL];
         }
+        this.#methods = this.methods;
         this.path = this.path || null;
         this.name = !this.name || !is_string(this.name) ? this.constructor.name : this.name;
         this.name = !is_string(this.name) ? null : this.name;
         this.priority = this.priority || 0;
         this.priority = !is_numeric(this.priority) ? 0 : parseInt(this.priority);
+    }
+
+    /**
+     * Request Options
+     *
+     * @type {{
+     *     methodEqual?: string,
+     *     headerContains?: {[key:string]: string},
+     *     headerEquals?: {[key:string]: string},
+     *     headerExists?: string[],
+     *     headerNotContains?: {[key:string]: string},
+     *     headerNotEquals?: {[key:string]: string},
+     *     headerNotExists?: string[],
+     *     headerNotStartsWith?: {[key:string]: string},
+     *     headerStartsWith?: {[key:string]: string},
+     *     queryContains?: {[key:string]: string},
+     *     queryEquals?: {[key:string]: string},
+     *     queryExists?: string[],
+     *     queryNotContains?: {[key:string]: string},
+     *     queryNotEquals?: {[key:string]: string},
+     *     pathEqual?: string,
+     *     validator?: (request: Request) => boolean|Promise<any>,
+     * }}
+     */
+    options = {};
+
+    /**
+     * Get options for validate
+     *
+     * @type {{
+     *     methodEqual?: string,
+     *     headerContains?: {[key:string]: string},
+     *     headerEquals?: {[key:string]: string},
+     *     headerExists?: string[],
+     *     headerNotContains?: {[key:string]: string},
+     *     headerNotEquals?: {[key:string]: string},
+     *     headerNotExists?: string[],
+     *     headerNotStartsWith?: {[key:string]: string},
+     *     headerStartsWith?: {[key:string]: string},
+     *     queryContains?: {[key:string]: string},
+     *     queryEquals?: {[key:string]: string},
+     *     queryExists?: string[],
+     *     queryNotContains?: {[key:string]: string},
+     *     queryNotEquals?: {[key:string]: string},
+     *     pathEqual?: string,
+     *     validator?: (request: Request) => boolean|Promise<any>
+     * }}
+     */
+    getOptions() {
+        if (!is_object(this.options)) {
+            this.options = {};
+        }
+        return this.options;
     }
 
     /**
@@ -306,11 +369,18 @@ export default class Controller {
                     }
                     methods.push(this.methods[key]);
                 }
-                methods = is_empty(methods) ? [ALL] : methods;
-                // make unique
-                this.methods = [...new Set(methods)];
+                this.methods = is_empty(methods) ? this.#methods : methods;
+                this.methods = [...new Set(this.methods)];
             } else {
-                this.methods = [ALL];
+                this.methods = this.#methods;
+            }
+            return this.methods;
+        }
+        if (is_array(this.methods)) {
+            // make unique
+            this.methods = [...new Set(this.methods)];
+            if (is_empty(this.methods)) {
+                this.methods = this.#methods;
             }
         }
         return this.methods;
@@ -372,6 +442,281 @@ export default class Controller {
     }
 
     /**
+     * Validate request
+     *
+     * @param {Request} request
+     * @return {boolean|Promise<boolean>}
+     */
+    validateRequest(request) {
+        return new Promise((resolve) => {
+            let methods = this.getMethods();
+            if (!is_array(methods)) {
+                this.methods = this.#methods;
+            }
+
+            // check if method is allowed
+            if (!methods.includes(ALL) && !methods.includes(request.method.toUpperCase())) {
+                resolve(false);
+                return false;
+            }
+            const options = this.getOptions();
+            if (is_function(options.validator)) {
+                resolve(options.validator(request));
+                return;
+            }
+            // check if options is empty
+            if (is_empty(options)) {
+                resolve(true);
+                return;
+            }
+
+            // method should equal
+            if (options.methodEqual
+                && is_string(options.methodEqual)
+                && options.methodEqual.trim() !== ''
+                && options.methodEqual.trim().toUpperCase() !== ALL
+                // should exist in methods
+                && (methods.includes(ALL) || methods.includes(options.methodEqual.trim().toUpperCase()))
+                && options.methodEqual.trim().toUpperCase() !== request.method.toUpperCase()
+            ) {
+                resolve(false);
+                return;
+            }
+
+            // path should equal
+            if (is_string(options.pathEqual) && options.pathEqual !== request.path) {
+                resolve(false);
+                return;
+            }
+            // header should contain
+            if (is_object(options.headerContains) && !is_empty(options.headerContains)) {
+                for (let key in options.headerContains) {
+                    if (!options.headerContains.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerContains[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (!request.headers.hasOwnProperty(key)) {
+                        resolve(false);
+                        return;
+                    }
+                    if (!request.headers[key].includes(options.headerContains[key])) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header value should equals
+            if (is_object(options.headerEquals) && !is_empty(options.headerEquals)) {
+                for (let key in options.headerEquals) {
+                    if (!options.headerEquals.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerEquals[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (!request.headers.hasOwnProperty(key)
+                        || request.headers[key] !== options.headerEquals[key]
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header should exist
+            if (is_array(options.headerExists) && !is_empty(options.headerExists)) {
+                for (let key of options.headerExists) {
+                    if (!is_string(key)) {
+                        continue;
+                    }
+                    key = NormalizeHeaderKey(key);
+                    if (!request.headers.hasOwnProperty(key)) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header shouldn't contains
+            if (is_object(options.headerNotContains) && !is_empty(options.headerNotContains)) {
+                for (let key in options.headerNotContains) {
+                    if (!options.headerNotContains.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerNotContains[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (!request.headers.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (request.headers[key].includes(options.headerNotContains[key])) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header value shouldn't equals
+            if (is_object(options.headerNotEquals) && !is_empty(options.headerNotEquals)) {
+                for (let key in options.headerNotEquals) {
+                    if (!options.headerNotEquals.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerNotEquals[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (!request.headers.hasOwnProperty(key)
+                        || request.headers[key] === options.headerNotEquals[key]
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header shouldn't exists
+            if (is_array(options.headerNotExists) && !is_empty(options.headerNotExists)) {
+                for (let key of options.headerNotExists) {
+                    if (!is_string(key)) {
+                        continue;
+                    }
+                    key = NormalizeHeaderKey(key);
+                    if (request.headers.hasOwnProperty(key)) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header value shouldn't start with
+            if (is_object(options.headerNotStartsWith) && !is_empty(options.headerNotStartsWith)) {
+                for (let key in options.headerNotStartsWith) {
+                    if (!options.headerNotStartsWith.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerNotStartsWith[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (request.headers.hasOwnProperty(key)
+                        && request.headers[key].startsWith(options.headerNotStartsWith[key])
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // header value should start with
+            if (is_object(options.headerStartsWith) && !is_empty(options.headerStartsWith)) {
+                for (let key in options.headerStartsWith) {
+                    if (!options.headerStartsWith.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.headerStartsWith[key])) {
+                        continue;
+                    }
+                    key = key.toLowerCase();
+                    if (!request.headers.hasOwnProperty(key)
+                        || !request.headers[key].startsWith(options.headerStartsWith[key])
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // query should contains
+            if (is_object(options.queryContains) && !is_empty(options.queryContains)) {
+                for (let key in options.queryContains) {
+                    if (!options.queryContains.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.queryContains[key])) {
+                        continue;
+                    }
+                    if (!request.query.hasOwnProperty(key)) {
+                        resolve(false);
+                        return;
+                    }
+                    if (!request.query[key].includes(options.queryContains[key])) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // query value should equals
+            if (is_object(options.queryEquals) && !is_empty(options.queryEquals)) {
+                for (let key in options.queryEquals) {
+                    if (!options.queryEquals.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.queryEquals[key])) {
+                        continue;
+                    }
+                    if (!request.query.hasOwnProperty(key)
+                        || !is_string(request.query[key])
+                        || request.query[key] !== options.queryEquals[key]
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // query should exists
+            if (is_array(options.queryExists) && !is_empty(options.queryExists)) {
+                for (let key of options.queryExists) {
+                    if (!is_string(key)) {
+                        continue;
+                    }
+                    if (!request.query.hasOwnProperty(key)) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+
+            // query shouldn't contains
+            if (is_object(options.queryNotContains) && !is_empty(options.queryNotContains)) {
+                for (let key in options.queryNotContains) {
+                    if (!options.queryNotContains.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.queryNotContains[key])) {
+                        continue;
+                    }
+                    if (!request.query.hasOwnProperty(key)) {
+                       continue;
+                    }
+                    if (!is_string(request.query[key]) || request.query[key].includes(options.queryNotContains[key])) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+            // query value shouldn't equals
+            if (is_object(options.queryNotEquals) && !is_empty(options.queryNotEquals)) {
+                for (let key in options.queryNotEquals) {
+                    if (!options.queryNotEquals.hasOwnProperty(key)) {
+                        continue;
+                    }
+                    if (!is_string(options.queryNotEquals[key])) {
+                        continue;
+                    }
+                    if (request.query.hasOwnProperty(key)
+                        && request.query[key] === options.queryNotEquals[key]
+                    ) {
+                        resolve(false);
+                        return;
+                    }
+                }
+            }
+
+            // try to resolve
+            resolve(true);
+        });
+    }
+
+    /**
      * Dispatch route
      *
      * @param {Request} request
@@ -384,6 +729,26 @@ export default class Controller {
         return new Promise(async (resolve, reject) => {
             if (this.#in_dispatch) {
                 resolve(null);
+                return;
+            }
+            try {
+                let shouldExecute = this.validateRequest(request);
+                if (is_async_function(shouldExecute)) {
+                    let err;
+                    shouldExecute = await shouldExecute.catch((e) => err = e);
+                    if (err instanceof Error) {
+                        reject(err);
+                        return;
+                    }
+                }
+                if (!shouldExecute) {
+                    next();
+                    resolve(null);
+                    return;
+                }
+            } catch (err) {
+                next(err);
+                reject(err);
                 return;
             }
             this.#in_dispatch = true;
